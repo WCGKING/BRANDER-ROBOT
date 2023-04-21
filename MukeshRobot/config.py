ba747d37a58502a5fd7925de3b23dc4536d2d2a8
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-04-21 21:42:12 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -14,19 +14,19 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "14050586" # integer value, dont use ""
-    API_HASH = "42a60d9c657b106370c79bb0a8ac560c"
+    API_ID = "22737009" # integer value, dont use ""
+    API_HASH = "049241d7262be025e7033a10d4577f00"
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = "1808943146" # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "THE_VIP_BOY"
+    OWNER_ID = "6258877205" # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "BRANDRD_21"
     SUPPORT_CHAT = "TG_FRIENDSS"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001723893843
+        -1001626004802
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001723893843
+        -1001626004802
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    MONGO_DB_URI= "mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_DB_URI= "mongodb+srv://Musicbot58:93529352@cluster0.n6wscri.mongodb.net/?retryWrites=true&w=majority"
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = ""  # needed for any database modules
     LOAD = []
@@ -39,13 +39,13 @@ class Config(object):
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = get_user_list("elevated_users.json", "1808943146")
+    DRAGONS = get_user_list("elevated_users.json", "5235126711")
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "1808943146")
+    DEV_USERS = get_user_list("elevated_users.json", "5235126711")
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    DEMONS = get_user_list("elevated_users.json", "1808943146")
+    DEMONS = get_user_list("elevated_users.json", "5235126711")
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGERS = get_user_list("elevated_users.json", "1808943146")
+    TIGERS = get_user_list("elevated_users.json", "5235126711")
     WOLVES = get_user_list("elevated_users.json", "whitelists")
     DONATION_LINK = "https://t.me/the_vip_boy" # EG, paypal
     CERT_PATH = None
