74e13a03f562a7736bcfa41bf22e5dbaacfa2116
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-04-22 12:20:05 +0530

    Update rules.py

--- MukeshRobot/modules/rules.py
+++ MukeshRobot/modules/rules.py
@@ -160,7 +160,7 @@ __help__ = """
  ‣ `/setrules <your rules here>`*:* set the rules for this chat.
  ‣ `/clearrules`*:* clear the rules for this chat.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚](https://t.me/BRANDRD_21)............☆
 """
 
 __mod_name__ = "⚡Rᴜʟᴇs⚡"
