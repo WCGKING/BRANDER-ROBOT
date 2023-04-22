2ebaa6bcede5657db3129735f0c833166a94199d
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-04-22 12:33:36 +0530

    Update debug.py

--- MukeshRobot/modules/debug.py
+++ MukeshRobot/modules/debug.py
@@ -71,6 +71,6 @@ __help__ = """
 /logs    ᴛᴏ ɢᴇᴛ ʜᴇʀᴜᴋᴏ ʟᴏɢs
 /debug ᴛᴏ ᴏɴ / ᴏғғ ᴅᴇʙᴜɢ 
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆"""
+☆............𝙱𝚈 » [𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚](https://t.me/BRANDRD_21)............☆"""
 __command_list__ = ["debug"]
 __handlers__ = [DEBUG_HANDLER]
