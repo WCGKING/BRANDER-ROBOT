bb8bfa269085bcb8fa47ffec8a7206315b66315b
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-06-04 12:23:57 +0530

    Update gettime.py

--- MukeshRobot/modules/gettime.py
+++ MukeshRobot/modules/gettime.py
@@ -99,7 +99,7 @@ __help__ = """
 
 💡 ᴇx:- /time ɪɴ *:* ɪᴛ ᴡɪʟʟ sʜᴏᴡs ɪɴᴅɪᴀɴ ᴄᴜʀʀᴇɴᴛ ᴛɪᴍᴇ ᴀɴᴅ ᴅᴀᴛᴇ..
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚](https://t.me/BRANDRD_21)............☆
 """
 
 TIME_HANDLER = DisableAbleCommandHandler("time", gettime)
