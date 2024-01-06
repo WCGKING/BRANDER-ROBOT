002a47bdc89a7f7385e31b01e35e5a559fbd8d52
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-01-06 14:02:59 +0530

    encrypt.py

--- MukeshRobot/modules/encrypt.py
+++ MukeshRobot/modules/encrypt.py
@@ -1,27 +1,47 @@
 import secureme
-
-from MukeshRobot.events import register
-
-
-@register(pattern="^/encrypt ?(.*)")
-async def hmm(event):
-    if event.reply_to_msg_id:
-        lel = await event.get_reply_message()
-        cmd = lel.text
-    else:
-        cmd = event.pattern_match.group(1)
-    Text = cmd
-    k = secureme.encrypt(Text)
-    await event.reply(k)
+from pyrogram import filters
+from MukeshRobot import pbot as mukesh
 
 
-@register(pattern="^/decrypt ?(.*)")
-async def hmm(event):
-    if event.reply_to_msg_id:
-        lel = await event.get_reply_message()
-        ok = lel.text
-    else:
-        ok = event.pattern_match.group(1)
-    Text = ok
-    k = secureme.decrypt(Text)
-    await event.reply(k)
+@mukesh.on_message(filters.command("encrypt"))
+async def encyrpt(bot, message):
+    if len(message.command) < 2:
+        return await message.reply_text("**Example:**\n\n`/encyrpt India`")
+    m = message.text.split(' ',1)[1]
+    try:
+        Secure = secureme.encrypt(m)
+        
+        await message.reply_text(f"`{Secure}`")
+        
+
+    except Exception as e:
+        await message.reply_text(f"Error {e}")
+
+@mukesh.on_message(filters.command("decrypt"))
+async def decrypt(bot, message):
+    if len(message.command) < 2:
+        return await message.reply_text("**Example:**\n\n`/decrypt Nsinf`")
+    m = message.text.split(' ',1)[1]
+    try:
+        Decrypt = secureme.decrypt(m)
+        
+        await message.reply_text(f"`{Decrypt}`")
+        
+
+    except Exception as e:
+        await message.reply_text(f"{e}")
+
+
+__mod_name__ = "ᴘᴀssᴡᴏʀᴅ"
+
+__help__ = """
+*ᴄᴏɴᴠᴇʀᴛs*
+ ❍ /encrypt*:* ᴇɴᴄʀʏᴘᴛs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ
+ ❍ /decrypt*:* ᴅᴇᴄʀʏᴘᴛs ᴘʀᴇᴠɪᴏᴜsʟʏ ᴇᴄʀʏᴘᴛᴇᴅ ᴛᴇxᴛ
+ ❍ /encode*:* ᴇɴᴄᴏᴅᴇ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ
+ ❍ /decode*:* ᴅᴇᴄᴏᴅᴇ ᴘʀᴇᴠɪᴏᴜsʟʏ ᴇᴄʀʏᴘᴛᴇᴅ ᴛᴇxᴛ
+ ❍ /morseencode*:* Mᴏʀsᴇ Eɴᴄᴏᴅᴇ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ
+ ❍ /morsedecode*:* ᴅᴇᴄʀʏᴘᴛs ᴘʀᴇᴠɪᴏᴜsʟʏ ᴇᴄʀʏᴘᴛᴇᴅ ᴛᴇxᴛ
+ ❍ /password *:*  ɢɪᴠᴇ ʟᴇɴɢᴛʜ ᴏғ ᴘᴀssᴡᴏʀᴅ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ
+ ❍ /uselessfact *:* Gᴇɴᴇʀᴀᴛᴇ  Rᴀᴍᴅᴏᴍ Usᴇʟᴇss Fᴀᴄᴛ
+"""
