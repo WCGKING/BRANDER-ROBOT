d36125a12d0c9cfd0c5f2703326e3fd1ba94f267
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-01-06 13:55:13 +0530

    pastebin.py

--- MukeshRobot/utils/pastebin.py
+++ MukeshRobot/utils/pastebin.py
@@ -18,7 +18,7 @@ def _netcat(host, port, content):
 
 async def paste(content):
     url ="https://pastebin.com/api/api_post.php"
-    data = {"api_dev_key":"9Rfu50iV5l3EuRWATw7EDLuC37RED-C4","api_paste_code": content,"api_option": "kingpaste"}
+    data = {"api_dev_key":"9Rfu50iV5l3EuRWATw7EDLuC37RED-C4","api_paste_code": content,"api_option": "paste"}
     response = requests.post(url, data=data)
     link=response.text
     return link
