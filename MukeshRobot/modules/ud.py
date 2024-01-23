4bdfb4fff2cd229bf118511764393d361f9316ca
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-01-23 13:00:21 +0530

    Update ud.py

--- MukeshRobot/modules/ud.py
+++ MukeshRobot/modules/ud.py
@@ -27,8 +27,8 @@ dispatcher.add_handler(UD_HANDLER)
 __help__ = """
 » /ud (text) *:* sᴇᴀʀᴄʜs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ ᴏɴ ᴜʀʙᴀɴ ᴅɪᴄᴛɪᴏɴᴀʀʏ ᴀɴᴅ sᴇɴᴅs ʏᴏᴜ ᴛʜᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ.
 
-☆............𝙱𝚈 » [𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚](https://t.me/BRANDRD_21)............☆
+☆............𝙱𝚈 » [𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚](https://t.me/BRANDEDKING82)............☆
 """
-__mod_name__ = "⚡Uʀʙᴀɴ⚡"
+__mod_name__ = "Uʀʙᴀɴ"
 __command_list__ = ["ud"]
 __handlers__ = [UD_HANDLER]
