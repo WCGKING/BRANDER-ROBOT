59d2279592903d4468325c0c49c4d1d7e0adbf69
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-08-09 16:57:07 +0530

    Update alive.py

--- MukeshRobot/modules/alive.py
+++ MukeshRobot/modules/alive.py
@@ -1,16 +1,41 @@
 import asyncio
 from platform import python_version as pyver
-
+from pyrogram.enums import ChatType
 from pyrogram import __version__ as pver
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from telegram import __version__ as lver
 from telethon import __version__ as tver
+from MukeshRobot.modules.no_sql import add_served_chat,save_id
+from MukeshRobot import SUPPORT_CHAT, pbot,BOT_USERNAME, OWNER_ID,BOT_NAME,START_IMG
 
-from MukeshRobot import SUPPORT_CHAT, pbot,BOT_USERNAME, OWNER_ID
+async def member_permissions(chat_id: int, user_id: int):
+    perms = []
+    member = (await pbot.get_chat_member(chat_id, user_id)).privileges
+    if not member:
+        return []
+    if member.can_post_messages:
+        perms.append("can_post_messages")
+    if member.can_edit_messages:
+        perms.append("can_edit_messages")
+    if member.can_delete_messages:
+        perms.append("can_delete_messages")
+    if member.can_restrict_members:
+        perms.append("can_restrict_members")
+    if member.can_promote_members:
+        perms.append("can_promote_members")
+    if member.can_change_info:
+        perms.append("can_change_info")
+    if member.can_invite_users:
+        perms.append("can_invite_users")
+    if member.can_pin_messages:
+        perms.append("can_pin_messages")
+    if member.can_manage_video_chats:
+        perms.append("can_manage_video_chats")
+    return perms
 
 PHOTO = [
-    "https://telegra.ph/file/d2a23fbe48129a7957887.jpg",
+     "https://telegra.ph/file/d2a23fbe48129a7957887.jpg",
     "https://telegra.ph/file/ddf30888de58d77911ee1.jpg",
     "https://telegra.ph/file/268d66cad42dc92ec65ca.jpg",
     "https://telegra.ph/file/13a0cbbff8f429e2c59ee.jpg",
@@ -19,53 +44,61 @@ PHOTO = [
 
 Mukesh = [
     [
-        InlineKeyboardButton(text="📍𝐎𝐰𝐧𝐞𝐫📍", url=f"tg://user?id={OWNER_ID}"),
-        InlineKeyboardButton(text="🍒𝐆𝐫𝐨𝐮𝐩🍒", url=f"https://t.me/{SUPPORT_CHAT}"),
+        InlineKeyboardButton(text="ɴᴏᴏʙ", user_id=OWNER_ID),
+        InlineKeyboardButton(text="ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}"),
     ],
     [
         InlineKeyboardButton(
-            text="☆ 𝐀𝐝𝐝 𝐌𝐞 𝐌𝐨𝐢 𝐋𝐮𝐯 ☆",
+            text="➕ᴀᴅᴅ ᴍᴇ ᴇʟsᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ➕",
             url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
         ),
     ],
 ]
 
-lol = "https://te.legra.ph/file/b5a457725d59cc7765dde.jpg"
 
 
 @pbot.on_message(filters.command("alive"))
 async def restart(client, m: Message):
     await m.delete()
     accha = await m.reply("⚡")
-    await asyncio.sleep(0.5)
+    await asyncio.sleep(0.2)
     await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ..")
-    await asyncio.sleep(0.5)
-    await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ......")
-    await asyncio.sleep(0.5)
-    await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ..")
-    await asyncio.sleep(0.5)
-    await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ......")
+
     await accha.delete()
-    await asyncio.sleep(0.5)
+    await asyncio.sleep(0.3)
+    
     umm = await m.reply_sticker(
-        "CAACAgUAAxkBAAECsn1kOUfJdJQ0Y-y8Ab2HPPYDz8dUZAAC9ggAAmXzyFV-diC545uQBS8E"
+        "CAACAgUAAxkDAAJHbmLuy2NEfrfh6lZSohacEGrVjd5wAAIOBAACl42QVKnra4sdzC_uKQQ"
     )
     await umm.delete()
-    await asyncio.sleep(0.8)
+    owner=await pbot.get_users(OWNER_ID)
     await m.reply_photo(
-        lol,
-        caption=f"""**🌷ʜᴇʏ, ɪ ᴀᴍ 『[𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚](f"t.me/{BOT_USERNAME}")』🎄**
-   ╔═════ஜ۩۞۩ஜ════╗
-
-   ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 [𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚](https://t.me/BRANDRD_21)♨️
-
-   ╚═════ஜ۩۞۩ஜ════╝""",
-        reply_markup=InlineKeyboardMarkup(Mukesh),
+        START_IMG,
+        caption=f"""**ʜᴇʏ, ɪ ᴀᴍ 『[{BOT_NAME}](f"t.me/{BOT_USERNAME}")』**
+   ━━━━━━━━━━━━━━━━━━━
+  » **ᴍʏ ᴏᴡɴᴇʀ :** {owner.mention()}
+  
+  » **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{lver}`
+  
+  » **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tver}`
+  
+  » **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pver}`
+  
+  » **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{pyver()}`
+   ━━━━━━━━━━━━━━━━━━━""",
+        reply_markup=InlineKeyboardMarkup(Mukesh)
     )
-__mod_name__ = "♨️ᴀʟɪᴠᴇ♨️"
-__help__ = """
 
-*ᴜsᴇʀ ᴄᴏᴍᴍᴀɴᴅs*:
-» /alive*:* ᴛᴏ ᴄʜᴇᴀᴋ ❓  ɪ ᴀᴍ ᴀʟɪᴠᴇ ᴏʀ ɴᴏᴛ?
+@pbot.on_message(group=1)
+async def save_statss(_, m):
+    try:
+        if m.chat.type == ChatType.PRIVATE:
+            save_id(m.from_user.id)
+        elif m.chat.type==ChatType.SUPERGROUP:
+            add_served_chat(m.chat.id)
+        else:
+            add_served_chat(m.chat.id)        
 
-☆............𝙱𝚈 » [𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚](https://t.me/BRANDRD_21)............☆"""
+    except Exception as e:
+        pass
+       # await _.send_message(OWNER_ID,f"db error {e}
