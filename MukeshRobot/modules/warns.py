4484084ca5a4865dd5265a2d979c39539a72687b
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-08-09 19:34:43 +0530

    Update warns.py

--- MukeshRobot/modules/warns.py
+++ MukeshRobot/modules/warns.py
@@ -521,51 +521,35 @@ __help__ = """
 
 __mod_name__ = "Wᴀʀɴs"
 
-WARN_HANDLER = CommandHandler(
-    ["warn", "dwarn"], warn_user, filters=Filters.chat_type.groups, run_async=True
-)
+WARN_HANDLER = CommandHandler(["warn", "dwarn"], warn_user, filters=Filters.group)
 RESET_WARN_HANDLER = CommandHandler(
     ["resetwarn", "resetwarns"],
     reset_warns,
-    filters=Filters.chat_type.groups,
-    run_async=True,
+    filters=Filters.group,
 )
-CALLBACK_QUERY_HANDLER = CallbackQueryHandler(
-    button, pattern=r"rm_warn", run_async=True
-)
-MYWARNS_HANDLER = DisableAbleCommandHandler(
-    "warns", warns, filters=Filters.chat_type.groups, run_async=True
-)
-ADD_WARN_HANDLER = CommandHandler(
-    "addwarn", add_warn_filter, filters=Filters.chat_type.groups, run_async=True
-)
-
+CALLBACK_QUERY_HANDLER = CallbackQueryHandler(button, pattern=r"rm_warn")
+MYWARNS_HANDLER = DisableAbleCommandHandler("warns", warns, filters=Filters.group)
+ADD_WARN_HANDLER = CommandHandler("addwarn", add_warn_filter, filters=Filters.group)
 RM_WARN_HANDLER = CommandHandler(
     ["nowarn", "stopwarn"],
     remove_warn_filter,
-    filters=Filters.chat_type.groups,
-    run_async=True,
+    filters=Filters.group,
 )
 LIST_WARN_HANDLER = DisableAbleCommandHandler(
     ["warnlist", "warnfilters"],
     list_warn_filters,
-    filters=Filters.chat_type.groups,
+    filters=Filters.group,
     admin_ok=True,
-    run_async=True,
 )
 WARN_FILTER_HANDLER = MessageHandler(
-    CustomFilters.has_text & Filters.chat_type.groups,
+    CustomFilters.has_text & Filters.group,
     reply_filter,
-    run_async=True,
 )
-WARN_LIMIT_HANDLER = CommandHandler(
-    "warnlimit", set_warn_limit, filters=Filters.chat_type.groups, run_async=True
-)
+WARN_LIMIT_HANDLER = CommandHandler("warnlimit", set_warn_limit, filters=Filters.group)
 WARN_STRENGTH_HANDLER = CommandHandler(
     "strongwarn",
     set_warn_strength,
-    filters=Filters.chat_type.groups,
-    run_async=True,
+    filters=Filters.group,
 )
 
 dispatcher.add_handler(WARN_HANDLER)
